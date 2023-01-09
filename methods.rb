881712d9b71e08ef10a5c2ab6548a9ce7a085fc6
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-09 00:51:34 -0600

    commit tree e4fd96b9b84e512ac4f7178a98e3cf0951258500

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default("Sunny")
+
+def add(num1, num2)
+    sum = num1 + num2
+    sum
+end
+
+add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+   number.to_i / 2
+    
+end    
+
+# halve('two')
+halve(6)
+halve("six")
